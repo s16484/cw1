32b23782e557497fb51892ae9313646d5c15be5a
Michał Bartosiak <s16484@pjwstk.edu.pl>
2020-03-01 14:26:46 +0100

    Kod zadania2

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
@@ -8,7 +11,26 @@ namespace Cw1
         { 
             //zadanie 2
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(AssemblyLoadEventArgs[0]);
+            var result = await httpClient.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+\\.[a-z]+",
+                                      RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+            }
+            
+            //JS promise async/await
+            //JAVA Future
+            //C# Task async/await
+
+            Console.WriteLine("Koniec!");
         }
     }
 }
